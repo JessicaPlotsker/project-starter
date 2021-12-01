33d740c0c9cf81f20d2ca8127d471a45943c6a6d
Andrew Li <andrew.li0264@gmail.com>
2021-11-30 20:21:48 -0500

    added titles to seed.js

--- seed.js
+++ seed.js
@@ -8,7 +8,9 @@ const TITLES = [
     {id: 4, Name:'One Piece', Author: 'Oda Eiichiro', Volume: 85},
     {id: 5, Name:'Vagabond', Author: 'Inoue Takehiko ', Volume: 1},
     {id: 6, Name:'Monster', Author: 'Urasawa, Naoki', Volume: 6},
-
-    
-    
+    {id: 7, Name:'Fullmetal Alchemist', Author: 'Hiromu Arakawa', Volume: 4},
+    {id: 8, Name:'Gintama', Author: 'Hideaki Sorachi', Volume: 30},
+    {id: 9, Name:'Shingeki no Kyojin', Author: 'Hajime Isayama', Volume: 19},
+    {id: 10, Name:'Fruits Basket', Author: 'Natsuki Takaya', Volume: 23},
+    {id: 11, Name:'Tokyo Ghoul', Author: 'Sui Ishida', Volume: 3}
 ]
