71dee69c4a2dae8a0bfb924acc6cdde67208d68c
Lessie Quezada <Lessieq07@gmail.com>
2021-11-30 20:07:15 -0500

    Added a few manga titles

--- seed.js
+++ seed.js
@@ -2,5 +2,13 @@ const db = require('/api/models');
 const { Title, Library} = db;
 
 const TITLES = [
-    {id: 1, Name: 'Vinland Saga', Author: 'Yukimura, Makoto' , Volume: 10}, 
+    {id: 1, Name: 'Vinland Saga', Author: 'Yukimura Makoto' , Volume: 10}, 
+    {id: 2, Name:'Berserk', Author: 'Miura Kentarou', Volume: 5},
+    {id: 3, Name:'Jojo Bizarre Adventure Part 7: Steel Ball Run', Author: 'Araki Hirohiko', Volume: 4},
+    {id: 4, Name:'One Piece', Author: 'Oda Eiichiro', Volume: 85},
+    {id: 5, Name:'Vagabond', Author: 'Inoue Takehiko ', Volume: 1},
+    {id: 6, Name:'Monster', Author: 'Urasawa, Naoki', Volume: 6},
+
+    
+    
 ]
